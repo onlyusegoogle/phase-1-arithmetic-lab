21cb20e604413e106fd5f7f984da37249b8a920f
mason white <masondwhite1@gmail.com>
2022-09-20 15:53:12 -0600

    commit tree 10069bc4b0dba5827d052bb518bd9ab533663fca

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 25
+const num4 = 7
+
+const multiply = num1 * num2
+
+const random = Math.round(Math.random() * 10)
+
+const mod = num3 % num4
+
+const max = Math.max(1,10,11,12,20)
